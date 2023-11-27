a1591adfa84f2b4bdd9aaaa58e1b4cea1a53f3d7
hung1321 <98698290+hung1321@users.noreply.github.com>
2023-11-27 21:05:52 +0700

    Update urls.py

--- recogniztion/recogniztion/urls.py
+++ recogniztion/recogniztion/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("insect.urls")),
+    path(r'^',include("insect.urls")),
 ]
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
